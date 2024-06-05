0cc2ac4b08a6ae21bd21ec46db2ce2ab6777abd3
korsar011 <vic014@yandex.ru>
2024-06-05 19:13:42 +0300

    Удалил неиспользуемую зависимость

--- homework-02-annotation/src/test/java/ru/otus/hw/service/StudentServiceImplTest.java
+++ homework-02-annotation/src/test/java/ru/otus/hw/service/StudentServiceImplTest.java
@@ -1,6 +1,5 @@
 package ru.otus.hw.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
