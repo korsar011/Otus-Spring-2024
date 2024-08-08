4268a1208e13031f124ec966b451ec105b6a774b
korsar011 <vic014@yandex.ru>
2024-08-08 08:05:09 +0300

    Добавил удаление комментов

--- homework-09-MVC/src/main/java/ru/otus/hw/services/BookServiceImpl.java
+++ homework-09-MVC/src/main/java/ru/otus/hw/services/BookServiceImpl.java
@@ -7,6 +7,7 @@ import ru.otus.hw.exceptions.EntityNotFoundException;
 import ru.otus.hw.models.Book;
 import ru.otus.hw.repositories.AuthorRepository;
 import ru.otus.hw.repositories.BookRepository;
+import ru.otus.hw.repositories.CommentRepository;
 import ru.otus.hw.repositories.GenreRepository;
 
 import java.util.List;
@@ -20,6 +21,9 @@ public class BookServiceImpl implements BookService {
     private final GenreRepository genreRepository;
 
     private final BookRepository bookRepository;
+
+    private final CommentRepository commentRepository;
+
 
     @Transactional(readOnly = true)
     @Override
@@ -48,6 +52,8 @@ public class BookServiceImpl implements BookService {
     @Transactional
     @Override
     public void deleteById(long id) {
+        commentRepository.deleteByBookId(id);
+
         bookRepository.deleteById(id);
     }
 
